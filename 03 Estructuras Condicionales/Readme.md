11f36de5d88385fb7fe1145c81886c26787b7e53
Gastón <rosales.gaston1@gmail.com>
2025-04-07 15:52:03 -0300

    readme incoroporado

---
+++ 03 Estructuras Condicionales/Readme.md
@@ -0,0 +1,7 @@
+### Trabajo practivo n3 ###
+
+Alumno: Rosales Gastón
+Comisión: 21
+
+Descripción: 
+Trabajo realizado en el marco de la carrera Tecnicatura en programación a distancia dictado por UTN FRSN.
--- 03 Estructuras Condicionales/tp3 - Condicionales.py
+++ 03 Estructuras Condicionales/tp3 - Condicionales.py
@@ -171,13 +171,34 @@ elif magnitud_terremoto >= 7:
 #del año es y qué día es. El programa deberá utilizar esa información para imprimir por pantalla
 #si el usuario se encuentra en otoño, invierno, primavera o verano.
 
-hemisferio = input("Ingrese el hemisferio donde se encuentra (N/S) : ")
-mes =  input("Ingrese el mes del año: ")
+hemisferio = input("Ingrese el hemisferio donde se encuentra (N/S) : ").capitalize();
+mes =  input("Ingrese el mes del año: ").capitalize()
 dia = int(input("Ingrese el día: "))
-calendario = (20,21,22,23,24,25,26,27,28,29,30,31,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19)
+#calendario = (20,21,22,23,24,25,26,27,28,29,30,31,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19)
 
-if mes == "Diciembre" or mes == "Enero" or mes == "Febrero" or mes == "Marzo" and (dia<1 or dia == 21) and hemisferio=='S':
-    print ("verano") 
-else:
-    print("invierno")
+verano_sur = ('Enero', 'Febrero')
+otoño_sur = ('Abril', 'Mayo')
+invierno_sur = ('Julio', 'Agosto')
+primavera_sur = ('Octubre','Noviembre')
 
+if hemisferio == 'S':
+    if (mes == 'Diciembre' and dia >= 21 ) or ((mes in verano_sur and dia >= 1 and dia <= 30) or (mes == 'Marzo' and dia <= 20)):
+        print ('Estas en Verano')
+    if (mes == 'Marzo' and dia >= 21) or ((mes in otoño_sur and dia >= 1 and dia <= 30) or (mes == 'Junio' and dia <=20)):
+        print('Estas en Otoño')
+    if (mes == 'Junio' and dia >= 21) or ((mes in invierno_sur and dia >= 1 and dia <= 30) or (mes == 'Septiembre' and dia <=20)):
+        print('Estas en Invierno')
+    if (mes == 'Septiembre' and dia >=21) or ((mes in primavera_sur and dia >= 1 and dia <= 30) or (mes == 'Diciembre' and dia <=20)):
+        print('Estas en Primavera')
+elif hemisferio == 'N':
+    if (mes == 'Diciembre' and dia >= 21 ) or ((mes in verano_sur and dia >= 1 and dia <= 30) or (mes == 'Marzo' and dia <= 20)):
+        print ('Estas en Inverno')
+    if (mes == 'Marzo' and dia >= 21) or ((mes in otoño_sur and dia >= 1 and dia <= 30) or (mes == 'Junio' and dia <=20)):
+        print('Estas en Primavera')
+    if (mes == 'Junio' and dia >= 21) or ((mes in invierno_sur and dia >= 1 and dia <= 30) or (mes == 'Septiembre' and dia <=20)):
+        print('Estas en Verano')
+    if (mes == 'Septiembre' and dia >=21) or ((mes in primavera_sur and dia >= 1 and dia <= 30) or (mes == 'Diciembre' and dia <=20)):
+        print('Estas en Otoño')
+
+
+print(f'los valores ingresados fueron ({hemisferio} - hemisferio), ({mes} - mes), ({dia} - dia)')
